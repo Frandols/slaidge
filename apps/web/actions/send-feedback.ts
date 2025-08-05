0236877f2ce9ace73b7c6bc8b1d58aabcfd254ac
Francisco <frandelosantoss12@gmail.com>
2025-08-05 11:26:39 -0300

    Use google user for feedback

--- apps/web/actions/send-feedback.ts
+++ apps/web/actions/send-feedback.ts
@@ -1,6 +1,9 @@
 'use server'
 
 import createSupabaseClient from '@/clients/factories/supabase'
+import requireAccessToken from '@/guards/require-access-token'
+import getUserProfile from '@/services/google/get-user-profile'
+import { cookies } from 'next/headers'
 
 /**
  * Send a feedback message.
@@ -8,20 +11,18 @@ import createSupabaseClient from '@/clients/factories/supabase'
  * @param message The message.
  */
 export default async function sendFeedback(message: string) {
+	const cookieStore = await cookies()
+
+	const accessToken = await requireAccessToken(cookieStore)
+
+	const userProfile = await getUserProfile(accessToken)
+
 	const supabase = await createSupabaseClient()
-
-	const {
-		data: { user },
-	} = await supabase.auth.getUser()
-
-	if (!user) throw new Error('UNAUTHENTICATED')
-
-	const userId = user.id
 
 	const { error } = await supabase.from('feedback').insert([
 		{
 			message,
-			user_id: userId,
+			user_id: userProfile.id,
 		},
 	])
 
